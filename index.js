6b6c40d06f702c6f82f7c55f11369e66b071c04d
Thazar-r <melchior.balthazar@student.moringaschool.com>
2024-07-09 12:42:24 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Convert response to JSON format
+  })
+  .then(data => {
+    renderBooks(data); // Call renderBooks function with the JSON data
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
 }
 
 function renderBooks(books) {
